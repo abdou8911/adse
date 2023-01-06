cfddcb90ca477f077ec47ca714756f9afe5f9c54
abdou8911 <63763082+abdou8911@users.noreply.github.com>
2023-01-06 16:45:15 +0100

    Update adsn.js

--- adsn.js
+++ adsn.js
@@ -1,1 +1,11 @@
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4895291677087020"crossorigin="anonymous"></script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4895291677087020"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="autorelaxed"
+     data-ad-client="ca-pub-4895291677087020"
+     data-ad-slot="1364265905"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
